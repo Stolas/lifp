ef9f17cc5b4449fd714da2b5873a1e046daf4c40
Robin Vossen <robin@codeinject.org>
2018-05-14 22:04:52 +0200

    Create default config included more tools

--- lifp.sh
+++ lifp.sh
@@ -70,7 +70,19 @@ function load_config
     CONFIG_FILE=~/.lifprc
     if [[ ! -e $CONFIG_FILE ]]; then
         touch $CONFIG_FILE
-        # Todo; Create example config
+        echo "#   _.,  Lazy Installer    ,._"> $CONFIG_FILE
+        echo "# ~(._\"@   For Pentesters @\"_.)~">> $CONFIG_FILE
+        echo "#   \" \"      -- Stolas     \" \"">> $CONFIG_FILE
+        echo "ROOT_DIR=\"$ROOT_DIR\"">> $CONFIG_FILE
+        echo "CORE_PATTERN=\"$CORE_PATTERN\"">> $CONFIG_FILE
+        echo "USE_ZYPPER=\"$USE_ZYPPER\"">> $CONFIG_FILE
+        echo "USE_PIP2=\"$USE_PIP2\"">> $CONFIG_FILE
+        echo "USE_PIP3=\"$USE_PIP3\"">> $CONFIG_FILE
+        echo "USE_GIT=\"$USE_GIT\"">> $CONFIG_FILE
+        echo "PIP2=\"$PIP2\"">> $CONFIG_FILE
+        echo "PIP3=\"$PIP3\"">> $CONFIG_FILE
+        echo "GIT_CLONE=\"$GIT_CLONE\"">> $CONFIG_FILE
+        echo "SKIP_CHECK=\"$SKIP_CHECK\"">> $CONFIG_FILE
         chmod 600 $CONFIG_FILE
     fi
     source $CONFIG_FILE
@@ -82,7 +94,6 @@ function run_lifp
     echo "~(._\"@   For Pentesters @\"_.)~"
     echo "  \" \"      -- Stolas     \" \""
 
-    echo -e "\e[32m\e[1m--] Current Configuration\e[0m"
     echo -e "\e[32m\e[1m--] Current Configuration\e[0m"
     load_config
     echo -n " :: root directory      " ; echo "$ROOT_DIR"
@@ -108,26 +119,29 @@ function run_lifp
         done
     fi
 
-    mkdir -p $ROOT_DIR/{Fuzzing,Exploitation,Cracking,Enumeration,SecLists,Reversing}
+    mkdir -p $ROOT_DIR/{Fuzzing,Exploitation,Cracking,Enumeration,Reversing}
 
     # Todo; Remove most of the zypper's
-    # Todo; Gef, GoBuster, Aircrack, SecLists, enum4linux, hashcat, hydra, ncrack, afl, z3, triton, EyeWitness, Impacket, Metasploit, umap, aircrack, hashcat, hydra, ncrack, binja, ripr, diphora, SecLists
+    # Todo; GoBuster, Aircrack, hashcat, hydra, afl, z3, triton, EyeWitness, Metasploit, umap, binja, ripr, diphora, proxychains, socat, Burp
 
     # Todo; Make configurable to ignore certain files w/o having to edit this.
-    install_zypper "DevTools" "cmake gcc"
+    install_zypper "DevTools" "cmake gcc gdb"
     install_zypper "Python2 DevChain" "python python-pip python-devel"
     install_zypper "Python3 DevChain" "python3 python3-pip python3-devel"
     install_zypper "OpenSSL Headers" libopenssl-devel
+    install_zypper "Libcurl Headers" libcurl-devel
     install_zypper "Python Curses" python-curses
     install_pip2   "PwnTools" pwntools
     install_zypper "Nmap Scanner" nmap
     install_zypper "Ncat" ncat
+    # Todo; Fix
+    # install_git    "Ncrack" https://github.com/nmap/ncrack.git Cracking/ncrack "./configure && make" 
     install_pip3   "MitM HTTP(S) Proxy" mitmproxy
     install_pip2   "Scapy Library Py2" scapy
     install_pip3   "Scapy Library Py3" scapy-python3
     install_zypper "ini_config LibraryaHeaders 64bit" libini_config-devel
     install_zypper "ini_config LibraryaHeaders 32bit" libini_config-devel-32bit
-    install_git    "Preeny Preload libs" https://github.com/zardus/preeny.git Exploitation/preeny "make -s -j4"
+    install_git    "Preeny Preload libs" https://github.com/zardus/preeny.git Exploitation/preeny "make -s"
     install_pip2   "Ropper" ropper
     install_pip2   "Keystone Engine" keystone-engine
     install_pip2   "Unicorn Engine" unicorn
@@ -135,14 +149,17 @@ function run_lifp
     install_zypper "John the Ripper" john
     install_git    "Sqlmap" https://github.com/sqlmapproject/sqlmap.git Exploitation/sqlmap
     install_zypper "Wireshark" wireshark-ui-qt
+    install_pip2   "WFuzz" wfuzz
+    install_git    "SecLists" https://github.com/danielmiessler/SecLists.git SecLists
+    install_git    "Enum4Linux" https://github.com/portcullislabs/enum4linux.git Enumeration/enum4linux
+    install_git    "Impacket" https://github.com/CoreSecurity/impacket.git Exploitation/impacket "pip2 install --user ." # Todo; This is a tad iffy
+    install_zypper "IDN Headers" libidn-devel
+    install_git    "Skipfish" https://github.com/spinkham/skipfish.git Exploitation/skipfish
+    install_git    "Radare2" https://github.com/radare/radare2.git Reversing/radare2 ./sys/user.sh
+    install_git    "GDB Enhanced Features (GEF)" https://github.com/hugsy/gef.git Reversing/gef  "./scripts/gef.sh && ./scripts/gef-extras.sh"
 
-    # Todo; Fix
     echo -e "\e[32m\e[1m--] Setting CorePattern\e[0m"
-    sudo sysctl -w kernel_core_pattern=$CORE_PATTERN
-
-    # Todo; Find a cleaner solution
-    echo -e "\e[32m\e[1m--] Installing GDB Enhanced Features (GEF) \e[0m"
-    wget -q -O- https://github.com/hugsy/gef/raw/master/gef.sh | sh
+    sudo sysctl -w kernel.core_pattern=$CORE_PATTERN
 }
 
 # Recommended to source the file, so the install_git command is available =)
